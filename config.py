6aa335fbc796db6d72d646014fe39ff0e0801eac
itsolver <angus@itsolver.net>
2018-07-18 00:50:53 +1000

    My config

--- config.py
+++ config.py
@@ -6,8 +6,14 @@ import getpass
 zendesk_subdomain = "itsolver.zendesk.com"
 zendesk_user = "angus@itsolver.net/token"
 destination_folder = "/Volumes/GoogleDrive/My Drive/1. Management/Zendesk/Backups"
+# Returns data that changed since the start time. See Usage Notes above.
+# to get epoch time on mac terminal use e.g. ``date -j -f "%d-%B-%y" 19-FEB-12 +%s``
+start_time = "1329575862"
+
 zendesk_secret = keyring.get_password("system", zendesk_user)
+
 if zendesk_secret is None:
-    zendesk_user = "angus@itsolver.net/token"
     zendesk_secret = getpass.getpass('Zendesk password or api_token: ') 
     keyring.set_password("system", zendesk_user, zendesk_secret)
+
+print("Config loaded!")
